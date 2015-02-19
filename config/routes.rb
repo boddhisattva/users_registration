4d1d7688a61b8478fe14404423759d1c4e158f8a
Mohnish G J <mail4mohnish@gmail.com>
2015-02-19 21:48:09 +0530

    user login and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+
+  root 'sessions#new'
+
   resources :users
+
+  get 'signup'  => 'users#new'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
