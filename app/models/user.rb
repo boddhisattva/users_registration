07274d5b7723d80afb9721f983a6927050a0c35f
Mohnish G J <mail4mohnish@gmail.com>
2015-02-19 17:48:20 +0530

    adds basic password setting mechanism for a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   validates :email, presence:true
+
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219115811) do
+ActiveRecord::Schema.define(version: 20150219120902) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",      limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "email",           limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "password_digest", limit: 255
   end
 
 end
