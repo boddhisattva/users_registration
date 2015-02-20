079b1df05423bfc419baf3fbaa4168c7d4994bce
Mohnish G J <mail4mohnish@gmail.com>
2015-02-20 08:47:12 +0530

    adds integration specs for signup via home page and signup page

--- spec/requests/home_page_spec.rb
+++ spec/requests/home_page_spec.rb
@@ -3,10 +3,17 @@ require 'rails_helper'
 RSpec.describe "HomePage", type: :request do
   describe "Home page" do
     it "should have the content 'User Registrations App'" do
-      # get home_pages_path
       visit root_path
       expect(page).to have_content('User Registrations App')
-      # expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "Sign up now! link" do
+    it "should go to the sign up page" do
+      visit root_path
+      click_link "Sign up now!"
+      expect(page).to have_content('Sign up')
+      expect(page).to have_content('Password Confirmation')
     end
   end
 end
