cbb331b58365613214d43d19084bd04c5c3b8edb
Mohnish G J <mail4mohnish@gmail.com>
2015-02-20 12:18:00 +0530

    corrects typo wrt welcome message name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Wecome aboard"
+      flash[:success] = "Welcome aboard"
       redirect_to @user
     else
       render 'new'
